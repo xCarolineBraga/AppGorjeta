b58aca673ce4821f0ed9f563c5deb84c38351468
CarolineBraga <carolinebraga.guimaraes@gmail.com>
2025-03-20 21:59:43 +0000

    - Implementação de ViewBinding - Interação com o RadioGroup

--- app/src/main/java/com/example/appgorjetai/Recebimento_Dados.kt
+++ app/src/main/java/com/example/appgorjetai/Recebimento_Dados.kt
@@ -2,43 +2,60 @@ package com.example.appgorjetai
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appgorjetai.databinding.ActivityRecebimentoDadosBinding
 import com.google.android.material.textfield.TextInputEditText
 
 const val KEY_NEXT_ACTIVITY ="Next.Activity"
 
 class Recebimento_Dados : AppCompatActivity() {
+    private lateinit var binding : ActivityRecebimentoDadosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recebimento_dados)
-
-        // Valor total conta
-        // número de pessoas
-        // porcentagem gorjeta
-        // 10% , 15% ou 20%
-        // Botão p Calcular
-        // Limpar
-
-        // Recuperar as views do layout
-        // FindViewById
-        //ViewBinding
-
-        val inicio = intent.getStringExtra(KEY_NEXT_ACTIVITY)
-
-        val edtTotal = findViewById<TextInputEditText>(R.id.tie_total)
-        val edtNumPeople = findViewById<TextInputEditText>(R.id.tie_num_people)
-        val btnClean = findViewById<Button>(R.id.btn_clean)
-        val btnDone = findViewById<Button>(R.id.btn_done)
-
-        btnClean.setOnClickListener {
-            println("ação 1" + edtTotal.text)
-            println("ação 2" + edtNumPeople.text)
-
+        binding = ActivityRecebimentoDadosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.rbOptionOne.setOnCheckedChangeListener { buttonView, isChecked ->
+            println("option One $isChecked")
         }
+        binding.rbOptionTwo.setOnCheckedChangeListener { buttonView, isChecked ->
+            println("option two $isChecked")
+        }
+        binding.rbOptionThree.setOnCheckedChangeListener { buttonView, isChecked ->
+            println("option three $isChecked")
+        }
+
+        binding.btnClean.setOnClickListener {
+            println("Carol 1" + binding.tieTotal.text)
+            println("Carol 2 " + binding.tieNumPeople.text)
+        }
+
+        binding.btnDone.setOnClickListener {  }
+
+
+        val inicio   = intent.getStringExtra(KEY_NEXT_ACTIVITY)
+
+
+
+
+
+
 
     }
 
 }
+
+// Valor total conta
+// número de pessoas
+// porcentagem gorjeta
+// 10% , 15% ou 20%
+// Botão p Calcular
+// Limpar
+
+// Recuperar as views do layout
+// FindViewById
+//ViewBinding
--- app/src/main/java/com/example/appgorjetai/Tela_Inicial.kt
+++ app/src/main/java/com/example/appgorjetai/Tela_Inicial.kt
@@ -4,22 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appgorjetai.databinding.ActivityTelaInicialBinding
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTelaInicialBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_inicial)
+        binding = ActivityTelaInicialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnStart = findViewById<Button>(R.id.start)
-
-        btnStart.setOnClickListener {
+        binding.btnStart.setOnClickListener {
             val intent = Intent(this,Recebimento_Dados::class.java)
             intent.putExtra(KEY_NEXT_ACTIVITY,"Next.Activity")
             startActivity(intent)
-
         }
     }
-
 
 }
